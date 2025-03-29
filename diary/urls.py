f624cd5419f1827c7917e32cd9ef0d85d5c20283
jia <jiagarodia18@gmail.com>
2025-03-29 13:45:52 +0530

    edit and delete needs to fix

--- diary/urls.py
+++ diary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
-    path('new/', views.new_entry, name='new_entry'),  # Add a new entry
+    path('new/', views.new_entry, name='new_entry'),  # Add a new entry  
     path('edit/<int:entry_id>/', views.edit_entry, name='edit_entry'),  # Edit an entry
     path('delete/<int:entry_id>/', views.delete_entry, name='delete_entry'),  # Delete an entry
 ]
